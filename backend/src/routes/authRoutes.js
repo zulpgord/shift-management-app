ad35014121d1a2bc770c4ba0269fa99388ba0ea1
zulpgord <sicurezza.cenci@gmail.com>
2026-04-12 16:18:34 +0200

    Add makeAdmin route to authRoutes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { register, login } = require('../controllers/authController');
+const { register, login, makeAdmin } = require('../controllers/authController');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 
+router.post('/make-admin', makeAdmin);
 module.exports = router;
