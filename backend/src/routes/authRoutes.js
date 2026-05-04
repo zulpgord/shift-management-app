28443fbb1a06049112999f82ea3e25e18b29404b
zulpgord <sicurezza.cenci@gmail.com>
2026-05-04 11:47:38 +0200

    Add reset password route to authRoutes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { register, login, makeAdmin, getUsers, updateUserRole } = require('../controllers/authController');
+const { register, login, makeAdmin, getUsers, updateUserRole, resetPassword } = require('../controllers/authController');
 const { authMiddleware, adminOnly } = require('../middleware/auth');
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 router.post('/make-admin', makeAdmin);
+router.post('/reset-password', resetPassword);
 router.get('/users', authMiddleware, adminOnly, getUsers);
 router.put('/users/:id/role', authMiddleware, adminOnly, updateUserRole);
 
