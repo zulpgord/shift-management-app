8c1f18bd2bd0e967e1461c624eb9b93c5a6d3410
zulpgord <sicurezza.cenci@gmail.com>
2026-05-11 08:58:28 +0200

    Routes: aggiungi PATCH cancel e DELETE per turni

--- backend/src/routes/shiftRoutes.js
+++ backend/src/routes/shiftRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { getShifts, createShift, updateShift, deleteShift } = require('../controllers/shiftController');
+const { getShifts, createShift, updateShift, cancelShift, deleteShift } = require('../controllers/shiftController');
 const { authMiddleware, adminOnly } = require('../middleware/auth');
+
 const router = express.Router();
 
 router.get('/', authMiddleware, getShifts);
 router.post('/', authMiddleware, adminOnly, createShift);
 router.put('/:id', authMiddleware, adminOnly, updateShift);
+router.patch('/:id/cancel', authMiddleware, adminOnly, cancelShift);
 router.delete('/:id', authMiddleware, adminOnly, deleteShift);
 
 module.exports = router;
